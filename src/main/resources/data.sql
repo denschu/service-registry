4fd9a4d23612c9a45d1f9cb81736d06d4f380db2
Dennis Schulte <mail@dennis-schulte.de>
2014-04-09 22:01:19 +0200

    Endpoints

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-insert into Service (id, name, version, url, message, active) values (1, 'service1', '1.0', 'http://localhost:8080/service1', 'Service not available.', false);
-insert into Service (id, name, version, url, message, active) values (2, 'service1', '2.0', 'http://localhost:8080/service1', 'Service not available.', true);
-insert into Service (id, name, version, url, message, active) values (3, 'service2', '1.0', 'http://localhost:8080/service2', 'Service not available.', true);
-insert into Service (id, name, version, url, message, active) values (4, 'service3', '1.0', 'http://localhost:8080/service3', null, false);
+insert into Service (id, name, version, url, message, active) values (1, 'service1', '1.0', 'localhost:8082/service1', 'Service not available.', false);
+insert into Service (id, name, version, url, message, active) values (2, 'service1', '2.0', 'localhost:8082/service1', 'Service not available.', true);
+insert into Service (id, name, version, url, message, active) values (3, 'service2', '1.0', 'localhost:8082/service2', 'Service not available.', true);
+insert into Service (id, name, version, url, message, active) values (4, 'service3', '1.0', 'localhost:8082/service3', null, false);
