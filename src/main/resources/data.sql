2400b264bfbf75d4baacc10ea434d7987d0c07a7
Dennis Schulte <mail@dennis-schulte.de>
2014-04-14 12:56:07 +0200

    RPM removed

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,2 @@
-insert into Service (id, name, version, url, message, active) values (1, 'service1', '1.0', 'localhost:8082/service1', 'Service not available.', false);
-insert into Service (id, name, version, url, message, active) values (2, 'service1', '2.0', 'localhost:8082/service1', 'Service not available.', true);
-insert into Service (id, name, version, url, message, active) values (3, 'service2', '1.0', 'localhost:8082/service2', 'Service not available.', true);
-insert into Service (id, name, version, url, message, active) values (4, 'service3', '1.0', 'localhost:8082/service3', null, false);
+insert into Service (id, name, version, url, message, active) values (1, 'example', '1.0', 'localhost:8082/example', 'Service not available.', true);
+insert into Service (id, name, version, url, message, active) values (2, 'example', '2.0', 'localhost:8082/example', 'Service not available.', true);
