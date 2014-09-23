ad15b5e8d305bbd98cba4ac591541e9ea600ef6b
Dennis Schulte <mail@dennis-schulte.de>
2014-09-23 11:29:00 +0200

    Add initscript

--- src/main/java/de/codecentric/Application.java
+++ src/main/java/de/codecentric/Application.java
@@ -1,6 +1,9 @@
 package de.codecentric;
 
+import java.io.File;
+
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.system.ApplicationPidListener;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
@@ -9,6 +12,9 @@ import org.springframework.context.annotation.ComponentScan;
 public class Application {
 
     public static void main(String[] args) {
-    	SpringApplication.run(Application.class, args);
+    	File pid = new File("/Users/denschu/git/service-registry/target/service-registry.pid");
+    	SpringApplication springApplication = new SpringApplication(Application.class);
+        springApplication.addListeners(new ApplicationPidListener(pid));
+        springApplication.run(args);
     }
 }
