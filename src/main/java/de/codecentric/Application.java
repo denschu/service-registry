e50bd5f6c3f4823d7f0d5eb005c619226cd6721c
Dennis Schulte <mail@dennis-schulte.de>
2014-10-10 14:12:23 +0200

    Pidfiles

--- src/main/java/de/codecentric/Application.java
+++ src/main/java/de/codecentric/Application.java
@@ -1,18 +1,21 @@
 package de.codecentric;
 
 import java.io.File;
+import java.util.Properties;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.actuate.system.ApplicationPidListener;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.util.StringUtils;
 
 @ComponentScan
 @EnableAutoConfiguration
 public class Application {
 
     public static void main(String[] args) {
-    	File pid = new File("/Users/denschu/git/service-registry/target/service-registry.pid");
+    	Properties argguments = StringUtils.splitArrayElementsIntoProperties(args, "=");
+    	File pid = new File(argguments.getProperty("--pid.file","target/pid.file"));
     	SpringApplication springApplication = new SpringApplication(Application.class);
         springApplication.addListeners(new ApplicationPidListener(pid));
         springApplication.run(args);
